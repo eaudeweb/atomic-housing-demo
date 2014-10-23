78b2778200c6e5b97a40f1bc97b3293bd6b8047a
Valentin Popescu <ppsc.valentin@gmail.com>
2014-10-23 16:43:34 +0300

    Added modals

---
+++ atomic_housing/static/js/main.js
@@ -0,0 +1,41 @@
+// $(document).ready(function () {
+// 	$("[data-popout='click']").on('click', function (e) {
+// 		e.preventDefault();
+// 		var target = $(this).data('target');
+// 		$(this).closest('.popout-container').children(target).addClass('active');
+// 		$(this).on('click', function (e) {
+// 			event.stopPropagation();		
+// 		});
+// 	});
+// });
+
+// $('html').on('click', function(e) {
+// 	$('.popout').removeClass('active');
+// });
+
+var openModal = function(elem) {
+	$(elem).addClass('active');
+	$('body').addClass('modal-open');
+
+	$(elem).on('click', function () {
+		event.stopPropagation();
+	});
+	$('.modal-container').on('click', function () {
+		closeModal();
+	});
+	$(document).keyup(function(e) {
+	  if (e.keyCode == 27) { 
+	  	closeModal();
+	  }
+	});
+}
+
+var closeModal = function() {
+	$('.modal.active').removeClass('active');
+	$('body').removeClass('modal-open');	
+}
+
+$("[data-modal]").on('click', function () {
+	var target = $(this).data('modal');
+	openModal(target);
+});
