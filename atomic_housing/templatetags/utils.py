a7bcb98003c1aa7b0d3c89be1aa2cb5ebe6857b2
Alex Eftimie <alex@eftimie.ro>
2014-10-27 22:22:40 +0200

    [misc] Show just the date. Use a |just_date filter.

--- atomic_housing/templatetags/utils.py
+++ atomic_housing/templatetags/utils.py
@@ -8,3 +8,10 @@ register = template.Library()
 @register.filter
 def filename(value):
     return os.path.basename(value.file.name)
+
+
+@register.filter
+def just_date(value):
+    if not value:
+        return ''
+    return value.date()
